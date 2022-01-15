02b7e297aa02af8deef09db9fe6c01a8aa8d7a5f
idahoenigmann <idahoney@gmx.at>
2022-01-15 13:36:05 +0100

    some changes

--- code/main.py
+++ code/main.py
@@ -2,18 +2,12 @@ import numpy as np
 import parse_matrix
 
 if __name__ == "__main__":
-    matrix = np.matrix([[5, 1], [1, 3]])
+    n = 5
+    matrix = parse_matrix.generate_rand_SPD_matrix(n)
+
     print(matrix)
 
-    l = parse_matrix.cholesky(matrix)
+    ssm = parse_matrix.SPDSkylineMatrix(matrix)
 
-    #print(l)
-    print(np.dot(l, l.T))
-
-    """
-    print(parse_matrix.is_square(matrix))
-    print(parse_matrix.is_symmetrical(matrix))
-    print(parse_matrix.is_positive_definite(matrix))
-
-    skyline_representation = parse_matrix.SkylineMatrix(matrix)
-    print(skyline_representation)"""
+    l1 = ssm.cholesky()
+    print(l1 * l1.T)
--- code/parse_matrix.py
+++ code/parse_matrix.py
@@ -2,6 +2,20 @@ import numpy as np
 import math
 
 
+def generate_rand_SPD_matrix(dimension):
+    matrix = np.empty((dimension, dimension))
+
+    while not (is_symmetrical(matrix) and is_positive_definite(matrix)):
+        matrix = np.random.rand(dimension, dimension)
+        matrix = 0.5 * (matrix + matrix.T) + np.identity(dimension)
+        # for i in range(dimension):
+        #     for j in range(i + 1, dimension):
+        #         scale = np.random.randint(-10, 10)
+        #         matrix[i, j] = matrix[i, j] * scale
+        #         matrix[j, i] = matrix[j, i] * scale
+    return matrix
+
+
 def is_square(matrix):
     return matrix.shape[0] == matrix.shape[1]
 
@@ -76,3 +90,57 @@ class SkylineMatrix:
                 k = k+1
 
         return matrix
+
+
+class SPDSkylineMatrix(SkylineMatrix):
+    def __init__(self, matrix):
+        if not is_symmetrical(matrix):
+            raise Exception("matrix must be symmetric")
+        if not is_positive_definite(matrix):
+            raise Exception("matrix must be positive definite")
+
+        self.dimension = len(matrix)
+
+        for i in range(self.dimension):
+            j = i
+            while j >= 0 and (not matrix[j, i] == 0):
+                self.values.append(matrix[j, i])
+                j = j-1
+
+            self.branch_lengths.append(i - j)
+
+    def __getitem__(self, item):
+        row, col = item
+        row, col = max(row, col), min(row, col)
+
+        idx = np.sum(self.branch_lengths[i] for i in range(row))
+
+        if row - col <= self.branch_lengths[row]:
+            return self.values[idx + row - col]
+        else:
+            return 0
+
+    def to_matrix(self):
+        matrix = np.empty((self.dimension, self.dimension))
+
+        k = 0
+        for i in range(self.dimension):
+            for j in range(self.branch_lengths[i]):
+                matrix[i - j, i] = self.values[k]
+                matrix[i, i - j] = self.values[k]
+                k = k + 1
+
+        return matrix
+
+    def cholesky(self):
+        l = np.empty((self.dimension, self.dimension))
+
+        for k in range(self.dimension):
+            s = np.sum(l[k, j] * l[k, j] for j in range(k - 1))
+            l[k, k] = math.sqrt(self[k, k] - s)
+
+            for i in range(k + 1, self.dimension):
+                s = np.sum(l[i, j] * l[k, j] for j in range(k - 1))
+                l[i, k] = (self[i, k] - s) / l[k, k]
+
+        return l
